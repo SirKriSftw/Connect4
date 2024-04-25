606012556bdceba3a9244b92806c1be5f50edd0c
Kristian Perez <kpere192@fiu.edu>
2024-04-25 14:46:26 -0400

    Made board constructor. Started with spots empty. An array with 7 arrays (each a column) each array with 6 elements (height). Also keeps track of current players turn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,25 @@
 class Board
 
+  def initialize
+    @spots = [
+      Array.new(6),
+      Array.new(6),
+      Array.new(6),
+      Array.new(6),
+      Array.new(6),
+      Array.new(6),
+      Array.new(6)
+    ]
+
+    @curr_player = 1
+  end
+
   def place(col)
   end
 
   def print_board
+  end
+
+  def win?
   end
 end
