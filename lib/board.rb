2c5d3fa14e835f020563d281550ddf8c735c5ffb
Kristian Perez <kpere192@fiu.edu>
2024-04-25 15:47:42 -0400

    Started work on other win conditions. Changed logic of last_piece to be equal to the last player who played

--- lib/board.rb
+++ lib/board.rb
@@ -57,20 +57,22 @@ class Board
 
   def win?
     return win_vertically? if win_vertically? != -1
+    return win_hortizontally? if win_hortizontally? != -1
+    return win_diagonally? if win_diagonally? != -1
     return -1
   end
 
   def win_vertically?
     # Only check if the most recently placed piece wins
+    @curr_player == 1 ? last_player = 2 : last_player = 1
     check = @spots[@most_recent_piece]
-    last_piece = 0
+    last_piece = last_player
     counter = 1
-    # First check vertical win
+
     check.each do |piece|
       if (piece == last_piece)
         counter += 1
       else
-        last_piece = piece
         counter = 1
       end
       if(counter == 4)
@@ -78,5 +80,20 @@ class Board
       end
     end
     return -1
+  end
+
+  def win_hortizontally?
+    # Only check if the most recently placed piece wins
+    @curr_player == 1 ? last_player = 2 : last_player = 1
+    check = @spots[@most_recent_piece].reverse.index(last_player)
+
+    last_piece = last_player
+    counter = 1
+
+    end
+    return -1
+  end
+
+  def win_diagonally?
   end
 end
