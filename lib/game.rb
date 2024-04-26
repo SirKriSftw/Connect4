f68a1e352ba783d79690bfd50be1b1e613a98902
Kristian Perez <kpere192@fiu.edu>
2024-04-26 13:05:58 -0400

    Made ask_options method in game class that asks for the various options to be enabled/disabled. By default all are false but if a player were to spam enter, ai will be enabled while all else is false

--- lib/game.rb
+++ lib/game.rb
@@ -3,5 +3,34 @@ require_relative "board"
 class Game
   def initialize
     @board = Board.new
+    @clear_game = false
+    @clear_turn = false
+    @ai = false
+    @random = false
+  end
+
+  def play
+    ask_options
+  end
+
+  def ask_options
+    puts "Hello, welcome to connect 4"
+    print "Would you like to clear the terminal after every game? (Y/N)"
+    input = gets.chomp
+    input.downcase == "y" ? @clear_game = true : @clear_game = false
+
+    print "Would you like to clear the terminal after every turn? (Y/N)"
+    input = gets.chomp
+    input.downcase == "y" ? @clear_turn = true : @clear_turn = false
+
+    print "Would you like to go against an AI? (Y/N)"
+    input = gets.chomp
+    input.downcase == "n" ? @ai = false : @ai = true
+
+    unless (ai == true)
+      print "Would you like to let two AIs go against each other? (Y/N)"
+      input = gets.chomp
+      input.downcase == "y" ? @random = true : @random = false
+    end
   end
 end
